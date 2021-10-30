c6d0289f7f97dcf4c64ab3cd1c87e5d139084a20
nightsplinter <paula.schwalm@t-online.de>
2021-10-31 00:06:57 +0200

    Add solution for day 4 part 1 (2015)

---
+++ 2015/days/04/src/PartOne.java
@@ -0,0 +1,36 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+/**
+ * --- Day 4: The Ideal Stocking Stuffer ---
+ *
+ * https://adventofcode.com/2015/day/4
+ **/
+
+public class PartOne {
+    public static void main(String[] args) throws Exception {
+        String input;
+        String hashtext;
+
+        String puzzleInput = "yzbqklnj";
+        int num = 0;
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+
+        do {
+            input = puzzleInput + String.valueOf(++num);
+
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            hashtext = no.toString(16);
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+        } while (!hashtext.substring(0, 5).equals("00000"));
+
+        System.out.println(num);
+    }
+}
