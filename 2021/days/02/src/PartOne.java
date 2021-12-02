fa384356575a1e0966ef9fff0e45dce15064658e
nightsplinter <paula.schwalm@t-online.de>
2021-12-02 06:41:11 +0100

    Add solution for day 2 part 1 (2021)

---
+++ 2021/days/02/src/PartOne.java
@@ -0,0 +1,69 @@
+
+/**
+ * --- Day 2: Dive! ---
+
+
+ *
+ * https://adventofcode.com/2021/day/2
+ **/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PartOne {
+    public static void main(String[] args) throws Exception {
+        Pattern forwardPattern = Pattern.compile("forward\\s(\\d+)");
+        Pattern downPattern = Pattern.compile("down\\s(\\d+)");
+        Pattern upPattern = Pattern.compile("up\\s(\\d+)");
+        boolean f = false;
+        boolean u = false;
+        Matcher find;
+        int depth = 0;
+        int horizontal = 0;
+
+        try {
+            Scanner scanner = new Scanner(new File("src/input.txt"));
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                Matcher forwardMatch = forwardPattern.matcher(line);
+                boolean forward = forwardMatch.matches();
+
+                Matcher downMatch = downPattern.matcher(line);
+                boolean down = downMatch.matches();
+
+                Matcher upMatch = upPattern.matcher(line);
+                boolean up = upMatch.matches();
+
+                if (forward) {
+                    find = forwardMatch;
+                    f = true;
+                } else if (down) {
+                    find = downMatch;
+                } else {
+                    find = upMatch;
+                    u = true;
+                }
+                int number = Integer.parseInt(find.group(1));
+
+                if (u) {
+                    depth -= number;
+                    u = false;
+                } else if (f) {
+                    horizontal += number;
+                    f = false;
+                } else {
+                    depth += number;
+                }
+            }
+            scanner.close();
+
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        System.out.println(depth * horizontal);
+    }
+}
